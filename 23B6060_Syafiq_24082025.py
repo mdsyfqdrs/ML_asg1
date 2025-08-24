5e79b582f7fbcb8d6bef0e95e5c4e8930db3ff40
Syafiq Idris <official.syafiqidris.07@gmail.com>
2025-08-25 00:59:35 +0800

    Final commit

--- 23B6060_Syafiq_24082025.py
+++ 23B6060_Syafiq_24082025.py
@@ -16,20 +16,20 @@ y = data['RATING'].values
 #Correlation Heatmap
 plt.figure(figsize=(12,10))
 sns.heatmap(data.corr(), annot=True, fmt=".2f", cmap="coolwarm")
-plt.title('Correlation Heatmap of Cereal Features')
+plt.title('Correlation Heatmap of the Cereal Features')
 plt.show()
 
-"""""
-#Pairplot for selected features vs Rating
-sns.pairplot(data[['CALORIES', 'PROTEIN', 'FAT', 'SODIUM', 'FIBER', 'CARBO', 'SUGARS', 'VITAMINS', 'RATING']], diag_kind='kde')
-plt.title('Pairplot of Cereal Nutritional Features & Rating')
+#Pairplot for Fiber vs Rating
+sns.pairplot(data[['FIBER', 'RATING']], diag_kind='kde')
+plt.title('Pairplot of Cereal Rating & Fiber', fontsize = 7, x=0, y=2.025)
+plt.show()
 plt.show()
 
-#Scatterplot: Calories vs Rating
+#Scatterplot: Fiber vs Rating
 plt.figure(figsize=(8,6))
-sns.scatterplot(x='SUGARS', y='RATING', data=data)
-plt.title('Scatter Plot: Cereal Rating vs Sugar')
-plt.xlabel('Sugar')
+sns.scatterplot(x='FIBER', y='RATING', data=data)
+plt.title('Scatter Plot: Cereal Rating vs Fiber')
+plt.xlabel('Fiber')
 plt.ylabel('Rating')
 plt.show()
 
@@ -38,6 +38,14 @@ plt.figure(figsize=(8,6))
 sns.histplot(data['RATING'], kde=True, bins=20)
 plt.title('Histogram of Cereal Rating')
 plt.xlabel('Rating')
+plt.ylabel('Frequency')
+plt.show()
+
+#Histogram of Fiber
+plt.figure(figsize=(8,6))
+sns.histplot(data['FIBER'], kde=True, bins=20)
+plt.title('Histogram of Cereal Fiber Content')
+plt.xlabel('Fiber')
 plt.ylabel('Frequency')
 plt.show()
 
@@ -56,9 +64,9 @@ mse = mean_squared_error(y, y_pred)
 plt.figure(figsize=(10,6))
 plt.scatter(x,y, color='blue', alpha=0.5, label='Data points')
 plt.plot(x,y_pred, color='red', label=f'Linear fit (R² = {r2:.2f})')
-plt.xlabel('Sugar Content')
+plt.xlabel('Fiber Content')
 plt.ylabel('Nutritional Rating')
-plt.title('Sugar Content vs Nutritional Rating with Linear Regression')
+plt.title('Fiber Content vs Nutritional Rating with Linear Regression')
 plt.legend()
 plt.grid(True)
 plt.show()
@@ -68,4 +76,3 @@ print(f'Slope (coefficient): {model.coef_[0]:.2f}')
 print(f'Intercept: {model.intercept_:.2f}')
 print(f'R2 score: {r2:.2f}')
 print(f'MSE: {mse:.2f}')
-"""
